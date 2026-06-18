a8dfb9b12ec3fd15550b3d52095733df4ad10e95
jh7647 <jhlee7647@gmail.com>
2026-06-19 07:52:38 +0900

    C

---
+++ 4_hd_260525_imp/run.sh
@@ -0,0 +1,6 @@
+# Subject : Linearization & Mode Shape with openfast
+# Date : 2026.05.23
+
+/home/jh7647/openFAST/build/glue-codes/openfast/openfast  ./Main.fst
+
+#/home/jh7647/openFAST/build/glue-codes/openfast/openfast  -VTKLin Main.ModeShapeVTK.viz 
